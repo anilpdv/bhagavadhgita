e493afdadb5decf050b139d651df8c9cb05fa8f3
anilpdv <pdvanil007@gmail.com>
2019-05-21 22:29:56 +0530

    changing routes to api

--- app.js
+++ app.js
@@ -1,106 +1,120 @@
-const express = require("express");
-const axios   = require("axios");
-const bodyParser = require("body-parser");
-const request = require("request");
-const path    = require("path");
-const util    = require("util");
+const express = require('express');
+const axios = require('axios');
+const bodyParser = require('body-parser');
+const request = require('request');
+const path = require('path');
+const util = require('util');
+const cors = require('cors');
 
 // : path
-var publicPath = path.join(__dirname ,"./public");
+var publicPath = path.join(__dirname, './public');
 // : variables
 var key = '';
 
 // : creating app instance
 const app = express();
 
+app.use(cors());
 // # middleware : bodyparser
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({extended: true}));
 // $ setting view engine : ejs
-app.set("view engine","ejs");
+app.set('view engine', 'ejs');
 // # middleware : static
 app.use(express.static(publicPath));
 
 // : api key generator
-function generateKey(){
-        request.post('https://bhagavadgita.io/auth/oauth/token',
-                   { form:{ 
-                    client_id : "zn5Jmj2xANpT2emcnUFgOlw1G6VO1bkAYeNnOXEA",
-                    client_secret:"1zuHyN1FbfNOX0Vn0NOgxcXqz6dxzhlZk09SdgRZ0ydDHIqzhk",
-                    grant_type:"client_credentials",
-                    scope :"verse chapter"}},function(err,res,body){
-                        body = JSON.parse(body);
-                        key = body.access_token;
-
-                    })        
-
+function generateKey() {
+  request.post(
+    'https://bhagavadgita.io/auth/oauth/token',
+    {
+      form: {
+        client_id: 'zn5Jmj2xANpT2emcnUFgOlw1G6VO1bkAYeNnOXEA',
+        client_secret: '1zuHyN1FbfNOX0Vn0NOgxcXqz6dxzhlZk09SdgRZ0ydDHIqzhk',
+        grant_type: 'client_credentials',
+        scope: 'verse chapter',
+      },
+    },
+    function(err, res, body) {
+      console.log(body);
+      body = JSON.parse(body);
+      key = body.access_token;
+    },
+  );
 }
 
+setInterval(generateKey, 300000);
 
-app.get('/root',function(req,res){
-    res.render("root");
+app.get('/root', function(req, res) {
+  res.render('root');
 });
 // @route : '/'
 // @desc  : get the root of the page
 // @access: public
-app.get('/',function(req,res,next){
-    axios.get("https://bhagavadgita.io/api/v1/chapters",{
-        params:{
-            access_token:`${key}`
-        }
+app.get('/', function(req, res, next) {
+  axios
+    .get('https://bhagavadgita.io/api/v1/chapters', {
+      params: {
+        access_token: `${key}`,
+      },
     })
-    .then(function(response){
-       // console.log(response);
-        res.render("index",{data:response});
+    .then(function(response) {
+      // console.log(response);
+      //res.render('index', {data: response});
+      res.status(200).json({data: response.data});
     })
-    .catch(function(error){
-        console.log("---root ---");
-        console.log(error);
-    })
-    
+    .catch(function(error) {
+      console.log('---root ---');
+      console.log(error);
+    });
 });
 
 // @route : '/chapter/show/:id'
 // @desc  : shows the chapter
 // @access: public
-app.get("/chapter/show/:id",function(req,res,next){
-    console.log(req.params.id);
-    let id = req.params.id;
-    axios.get("https://bhagavadgita.io/api/v1/chapters/"+id,{
-        params:{
-            access_token:`${key}`
-        }
+app.get('/chapter/show/:id', function(req, res, next) {
+  console.log(req.params.id);
+  let id = req.params.id;
+  axios
+    .get('https://bhagavadgita.io/api/v1/chapters/' + id, {
+      params: {
+        access_token: `${key}`,
+      },
     })
-    .then(function(response){
-        console.log(response.data);
-       res.render("show",{data:response.data});
+    .then(function(response) {
+      //res.render('show', {data: response.data});
+      res.json({data: response.data});
     })
-    .catch(function(error){
-        console.log(error);
+    .catch(function(error) {
+      console.log(error);
     });
 });
 
 // @route : '/chapters/show/:id/verses'
 // @desc  : shows the verses of each chapter
 // @access: public
-app.get("/chapter/show/:id/verses",function(req,res,next){
-    let id  = req.params.id;
-    let url = util.format("https://bhagavadgita.io/api/v1/chapters/%s/verses",id);
-    axios.get(url,{
-        params:{
-            access_token:`${key}`
-        }
+app.get('/chapter/show/:id/verses', function(req, res, next) {
+  let id = req.params.id;
+  let url = util.format(
+    'https://bhagavadgita.io/api/v1/chapters/%s/verses',
+    id,
+  );
+  axios
+    .get(url, {
+      params: {
+        access_token: `${key}`,
+      },
     })
-    .then(function(response){
-        console.log(response.data);
-        res.render("verses",{data:response.data});
+    .then(function(response) {
+      // res.render('verses', {data: response.data});
+      res.status(200).json({data: response.data});
     })
-    .catch(function(error){
-        console.log(error);
+    .catch(function(error) {
+      console.log(error);
     });
 });
 
 // : listen to port : 3000
-app.listen(3001,function(){
-    generateKey();
-    console.log("app start and running");
+app.listen(process.env.PORT || 3001, function() {
+  generateKey();
+  console.log('app start and running', '3001');
 });
